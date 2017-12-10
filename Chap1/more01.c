f9e6f215ec23558ea3c08801467344d92265a8e4
zhanglei1032 <1032465191@qq.com>
2017-12-10 16:45:35 +0800

    添加注释

--- Chap1/more01.c
+++ Chap1/more01.c
@@ -10,9 +10,11 @@ int see_more();
 
 int main(int argc, char const *argv[]) {
   FILE *fp;
+  //more后面不加参数
   if (argc == 1) {
     do_more(stdin);
   } else {
+    //循环打开more命令后面跟着的所有文件，执行do_more
     while (--argc) {
       if ((fp = fopen(* ++argv, "r")) != NULL) {
         do_more(fp);
@@ -27,13 +29,12 @@ int main(int argc, char const *argv[]) {
 
 
 void do_more(FILE *fp) {
-  /* code */
   char line[LINELEN];
   int num_of_lines = 0;
   int see_more(), reply;
-
+  //从fp指向的文件中读取LINELEN个字符到line数组中
   while ( fgets(line, LINELEN, fp)) {
-    /* code */
+    //输出pagelen行后，执行see_more,等待输入参数
     if (num_of_lines == PAGELEN) {
       reply = see_more();
       if ( reply == 0) {
@@ -41,7 +42,9 @@ void do_more(FILE *fp) {
       }
       num_of_lines -= reply;
     }
+    //每次循环都把line的内容输出，遇到错误退出
     if (fputs(line, stdout) == EOF) {
+      //fputs返回EOF写入失败
       exit(1);
     }
     num_of_lines++;
@@ -51,8 +54,10 @@ void do_more(FILE *fp) {
 
 int see_more(){
   int c;
+  //反白显示more？]
   printf("\033[7m more?]\033[m");
   while ((c = getchar()) != EOF) {
+  //按q退出，按空格再显示PAGELEN行，回车多显示一行
     if ( c == 'q') {
       return 0;
     }
