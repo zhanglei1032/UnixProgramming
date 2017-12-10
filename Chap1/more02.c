1b337f4a85797c7f14391efe36bb3213e2d2e9d2
zhanglei1032 <1032465191@qq.com>
2017-12-10 19:55:32 +0800

    more第二版实现

--- Chap1/more02.c
+++ Chap1/more02.c
@@ -1,5 +1,6 @@
-/* more
+/* more第二版
  * 读取并打印24行然后暂停
+ * 改善了第一版执行 ls /bin | more 时，数据显示24后，继续打印的bug
  */
 
 #include <stdio.h>
@@ -8,6 +9,7 @@
 void do_more(FILE *);
 int see_more();
 
+//主程序没变
 int main(int argc, char const *argv[]) {
   FILE *fp;
   //more后面不加参数
@@ -31,12 +33,21 @@ int main(int argc, char const *argv[]) {
 void do_more(FILE *fp) {
   char line[LINELEN];
   int num_of_lines = 0;
-  int see_more(), reply;
+  int see_more(FILE*), reply;
+  FILE *fp_tty;
+  //  /dev/tty是键盘和显示器的设备描述文件
+  fp_tty = fopen("/dev/tty", "r");
+
+  if ( fp_tty == NULL) {
+    exit(1);
+  }
   //从fp指向的文件中读取LINELEN个字符到line数组中
   while ( fgets(line, LINELEN, fp)) {
-    //输出pagelen行后，执行see_more,等待输入参数
+    //输出pagelen行后，执行see_more,等待输入参数，
+   //此处see_more从第一版的标准输入（可能是管道前命令的输出）变成了指定输入（此时为键盘）
     if (num_of_lines == PAGELEN) {
-      reply = see_more();
+      //see_more只能从键盘读取输入，修正了从管道输入数据时的bug
+      reply = see_more(fp_tty);
       if ( reply == 0) {
         break;
       }
@@ -52,11 +63,11 @@ void do_more(FILE *fp) {
 }
 
 
-int see_more(){
+int see_more(FILE *cmd){
   int c;
   //反白显示more？
   printf("\033[7m more? \033[m");
-  while ((c = getchar()) != EOF) {
+  while ((c = getc(cmd)) != EOF) {
   //按q退出，按空格再显示PAGELEN行，回车多显示一行
     if ( c == 'q') {
       return 0;
